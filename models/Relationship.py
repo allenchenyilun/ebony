d009cc19ddcdcdbd814ae4efe86909633059af55
Allen Chen <fuluohao@hotmail.com>
2022-07-07 17:10:03 -0500

    Initialize class"Relationship"

---
+++ models/Relationship.py
@@ -0,0 +1,5 @@
+class Relationship:
+    def __init__(self, item_before, item_after, logic):
+        self.item_before = item_before
+        self.item_after = item_after
+        self.logic = logic
